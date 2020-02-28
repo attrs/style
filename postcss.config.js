5b63074ac02852f64559aa4e4e23a3dbb8d3f7e6
joje <joje.attrs@gmail.com>
2020-02-29 01:20:30 +0900

    change based on postcss/tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+  plugins: [
+    require('tailwindcss')('./tailwindcss-config.js'),
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('postcss-custom-properties'),
+    require('postcss-preset-env')({ stage: 1 }),
+    require('autoprefixer'),
+  ]
 };
