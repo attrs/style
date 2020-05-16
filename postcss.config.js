c96af9d582087ec93b279ccec0c0850b5732a79d
joje <joje.attrs@gmail.com>
2020-05-17 03:13:01 +0900

    CTY-80: @attrs/style 구성 (tailwindcss 기반)

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,11 @@
-module.exports = {
+module.exports = (ctx) => ({
+  map: ctx.options.map,
   plugins: [
-    require('postcss-apply'),
-    require('postcss-import'),
-    require('postcss-parcel-import'), // import 문제 임시해결책: https://github.com/parcel-bundler/parcel/issues/1165
-    require('postcss-url'),
-    require('postcss-nested'),
-    require('postcss-custom-properties'),
+    require('postcss-import')({ root: ctx.file.dirname }),
+    require('postcss-url')({ url: 'copy', useHash: true }),
     require('postcss-preset-env')({ stage: 1 }),
-    require('autoprefixer'),
+    require('postcss-parcel-import'), // import 문제 임시해결책: https://github.com/parcel-bundler/parcel/issues/1165
     require('tailwindcss')('./tailwind.config.js'),
+    require('autoprefixer')
   ]
-};
+});
