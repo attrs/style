0a24341c28957507391ea735aadd0bd540614803
joje <joje.attrs@gmail.com>
2022-08-04 19:47:17 +0900

    ..

--- src/worklets/js-in-css.worklet.js
+++ src/worklets/js-in-css.worklet.js
@@ -1,13 +1,14 @@
 // See https://css-houdini.rocks/js-in-css/
 
-typeof registerPaint === 'function' && registerPaint(
-  'background-canvas',
-  class {
-    static get inputProperties() {
-      return ['--background-canvas'];
+typeof registerPaint === 'function' &&
+  registerPaint(
+    'background-canvas',
+    class {
+      static get inputProperties() {
+        return ['--background-canvas'];
+      }
+      paint(ctx, geom, properties) {
+        eval(properties.get('--background-canvas').toString())(ctx, geom, properties);
+      }
     }
-    paint(ctx, geom, properties) {
-      eval(properties.get('--background-canvas').toString())(ctx, geom, properties);
-    }
-  }
-);
+  );
